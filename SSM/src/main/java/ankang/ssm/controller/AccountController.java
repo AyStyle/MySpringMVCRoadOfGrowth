527252db9ca0122a5effa9a8afe84da1529899b4
ankang <ankang@soyoung.com>
2020-08-31 20:23:37 +0800

    SSM框架整合完毕

---
+++ SSM/src/main/java/ankang/ssm/controller/AccountController.java
@@ -0,0 +1,37 @@
+package ankang.ssm.controller;
+
+import ankang.ssm.pojo.Account;
+import ankang.ssm.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+/**
+ * @author: ankang
+ * @email: dreedisgood@qq.com
+ * @create: 2020-08-31
+ */
+@Controller
+@RequestMapping("/account")
+public class AccountController {
+
+    /*
+     * Spring和SpringMVC是有层次的（父子容器）
+     * Spring容器管理：service和dao对象
+     * SpringMVC容器管理：controller对象，且SpringMVC容器可以引用Spring容器中的对象
+     */
+
+    @Autowired
+    private AccountService accountService;
+
+    @RequestMapping("/queryAll")
+    @ResponseBody
+    public List<Account> queryAll() {
+        return accountService.queryAll();
+    }
+
+}
---
+++ SSM/src/main/java/ankang/ssm/controller/Controller.java
@@ -0,0 +1,9 @@
+package ankang.ssm.controller;
+
+/**
+ * @author: ankang
+ * @email: dreedisgood@qq.com
+ * @create: 2020-08-31
+ */
+public interface Controller {
+}
